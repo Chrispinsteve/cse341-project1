d3e1d7612b1c36577eb9c5f92eba7618b32b1118
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2025-01-29 11:55:27 -0500

    for render

--- data/database.js
+++ data/database.js
@@ -5,16 +5,19 @@ dotenv.config();
 let db;
 
 const initDb = async (callback) => {
+  // Check if the database is already initialized
   if (db) {
     console.log("Database is already initialized!");
     return callback(null, db);
   }
 
+  // Ensure the MONGODB_URL is set in the environment variables
   if (!process.env.MONGODB_URL) {
     console.error("MONGODB_URL is not set in environment variables.");
     return callback(new Error("Missing MONGODB_URL in environment variables."));
   }
 
+  // Attempt to connect to the database
   try {
     console.log("Attempting to connect to database...");
     const client = await MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
